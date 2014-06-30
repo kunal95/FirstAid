a37ba0c7c53fbd3668261a06c6b56b138f41fec8
kunal95 <kunsachdeva@gmail.com>
2014-07-01 01:09:31 +0530

    first

---
+++ js/javaBl.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#B_M_").hide(); $("#belowB_M").hide();
+    $("#B_M").click(function () { $("#B_M_").slideToggle(); $("#belowB_M").toggle(); });
+    $("#B_M_2").hide(); $("#belowB_M2").hide();
+    $("#B_M2").click(function () { $("#B_M_2").slideToggle(); $("#belowB_M2").toggle(); });
+    $("#B_M_3").hide(); $("#belowB_M3").hide();
+    $("#B_M3").click(function () { $("#B_M_3").slideToggle(); $("#belowB_M3").toggle(); });
+});
